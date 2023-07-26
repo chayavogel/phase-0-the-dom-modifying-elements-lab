1348da1d0cb9555f693bd7302f84e48759a759ed
chaya <chayavogel1@gmail.com>
2023-07-26 12:12:21 -0400

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.textContent = "Chaya Vogel is the champion"
+newHeader.id = "victory"
+document.body.append(newHeader)
